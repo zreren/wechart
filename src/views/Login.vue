38bbf60355f9ee62adea395d919a69fb8804b444
aei <3035816700@qq.com>
2021-08-23 19:36:24 +0800

    注册页面优化

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -26,41 +26,48 @@
    <div class="overlay-panel overlay-left">
     <h1>Welcome Back!</h1>
     <p>To keep connected with us please login with your personal info</p>
-    <button class="ghost" id="signIn">Sign In</button>
+    <button class="ghost" id="signIn" @click="signInChange">Sign In</button>
    </div>
    <div class="overlay-panel overlay-right">
     <h1>Hello, Friend!</h1>
     <p>Enter your personal details and start journey with us</p>
-    <button class="ghost" id="signUp" @click="singupchange">Sign Up</button>
+    <button class="ghost" id="signUp" @click="signUpChange">Sign Up</button>
    </div>
   </div>
  </div>
 </div>
-
-<footer>
- <p>
-  Created with <i class="fa fa-heart"></i> by
-  <a target="_blank" href="">Florin Pop</a>
-  - Read how I created this and how you can join the challenge
-  <a target="_blank" href="">here</a>.
- </p>
-</footer>
+    <footer>
+            @create by WeChart
+    </footer>
 </body>
 </template>
 
 <script>
+import { onMounted } from '@vue/runtime-core';
 export default {
-    name:'Login',
+    name:'Login',
     
     setup() {
-    singupchange(()=>{
-      const container = document.getElementById('container');
-      container.classList.add("right-panel-active");
-      // signInButton.addEventListener('click', () => {
-     // container.classList.remove("right-panel-active");
-      // });
-})
-      
+
+
+    let container
+
+    onMounted(()=>{
+        container = document.getElementById('container');
+    })
+
+    function signUpChange() {
+        container.classList.add("right-panel-active");
+    }
+    function signInChange() {
+        container.classList.remove("right-panel-active");
+    }
+     
+    return {
+        signUpChange,
+        signInChange
+    }
+     
     }
 }
 </script>
@@ -78,8 +85,7 @@ body {
  align-items: center;
  flex-direction: column;
  font-family: 'Montserrat', sans-serif;
- height: 100vh;
- margin: -20px 0 50px;
+ height: calc(100vh - 60px);
 }
 
 h1 {
@@ -227,8 +233,7 @@ input {
 
 .overlay {
  background: #FF416C;
- background: -webkit-linear-gradient(to right, #2F80ED80, #2F80ED);
- background: linear-gradient(to right, #2F80ED80, #2F80ED);
+ background: linear-gradient(to bottom, #dfe3e980, #0868e6);
  background-repeat: no-repeat;
  background-size: cover;
  background-position: 0 0;
@@ -292,29 +297,10 @@ input {
  width: 40px;
 }
 
-footer {
-    background-color: #222;
-    color: #fff;
-    font-size: 14px;
-    bottom: 0;
-    position: fixed;
-    left: 0;
-    right: 0;
-    text-align: center;
-    z-index: 999;
-}
-
-footer p {
-    margin: 10px 0;
-}
-
-footer i {
-    color: red;
-}
-
-footer a {
-    color: #3c97bf;
-    text-decoration: none;
+footer{
+    position: absolute;
+    bottom: 25px;
+    color: #c9d0da;
 }
 
 </style>

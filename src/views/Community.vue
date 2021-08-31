f03e672fc7712aa38e83b3c37201eb915de51586
tjustb185150735 <1031735010@qq.com>
2021-08-31 20:05:13 +0800

    广场的更改

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -25,33 +25,40 @@
             class="liStyle"
             v-for="(item, index) in this.dataList"
             :key="index"
-            @click="changeType(item.value)"
+            :class="selectedIndex == index ? 'active' : ''"
+            @click="changeType(item.name, index)"
           >
             {{ item.name }}
           </li>
         </ul>
       </div>
-    </div>
-    <!-- 搜索框 -->
-    <div class="searchpack">
-      <div class="search">
-        <input
-          type="text"
-          class="input"
-          placeholder="搜索关键字"
-          v-model="searchText"
-        />
+      <!-- 搜索框 -->
+      <div class="searchpack">
+        <div class="search">
+          <input
+            type="text"
+            class="input"
+            placeholder="搜索关键字"
+            v-model="searchText"
+          />
+        </div>
+        <i class="el-icon-search" style="" @click="search"></i>
       </div>
-      <i class="el-icon-search" style=""></i>
     </div>
-    <div class="divBody" v-show="show">
+    <!-- 视图 -->
+    <div class="divBodynull" v-if="infoData.length == 0">
+      <el-empty description="暂无数据" :image-size="200"></el-empty>
+    </div>
+    <div class="divBody" v-show="show" v-else>
       <div
         class="view"
         v-for="item in infoData"
         :key="item"
         @click="clickItem(item._id)"
       >
-        <div class="viewimg"><img :src="item.imageUrl" alt="" /></div>
+        <div class="viewimg">
+          <img :src="item.imageUrl" alt="数据加载失败，请重试.." />
+        </div>
         <div class="viewtext">
           <div>{{ item.title }}</div>
           <div>
@@ -61,67 +68,69 @@
         </div>
       </div>
     </div>
-    <el-dialog v-model="showDialog" width="90%" top="5vh">
+    <!-- 弹窗 -->
+    <el-dialog
+      v-model="showDialog"
+      width="90%"
+      top="5vh"
+      :destroy-on-close="true"
+    >
       <div class="dialogMain">
-        <!-- {{ dialogData }} -->
         <div class="dialogTitle">
           <!-- 头像 -->
           <div class="head">
-            <div class="head-img"></div>
+            <div class="head-img">
+              <img
+                class="img"
+                src="https://sf1-ttcdn-tos.pstatp.com/obj/larkcloud-file-storage/baas/qc5abu/2dedbafcccf601c4_1630335837047.png"
+                alt="头像加载失败"
+              />
+            </div>
           </div>
           <!-- 作者信息 -->
           <div class="author">
-            <div class="author_name"><h1>懒洋洋</h1></div>
+            <div class="author_name">{{ toRaw(dialogData[0].userName) }}</div>
             <div class="create_time">
-              <span style="font-size: 16px">发表时机：2021/08/29/10:08</span>
+              <span
+                >发表时间：
+                {{ toRaw(dialogData[0].createdAt.substr(0, 10)) }}
+              </span>
             </div>
           </div>
           <!-- 文章信息 -->
           <div class="message">
-            <div class="message_title"><h1>青青草原地图</h1></div>
+            <div class="message_title">{{ toRaw(dialogData[0].title) }}</div>
             <div class="message_type">
               <span>内容关键词：</span>
-              <el-tag type="info" size="mini">标签一</el-tag>
-              <el-tag type="info" size="mini">标签一</el-tag>
-              <el-tag type="info" size="mini">标签一</el-tag>
+              <el-tag
+                size="mini"
+                v-for="item in toRaw(dialogData[0].tags)"
+                :key="item"
+                >{{ item }}</el-tag
+              >
             </div>
           </div>
           <!-- like按钮 -->
           <div class="like">
-            <div class="button">
-              <i class="el-icon-star-on">&nbsp;Like</i>
-            </div>
+            <el-button
+              type="primary"
+              class="button"
+              size="mini"
+              @click="like(toRaw(dialogData[0]._id))"
+            >
+              <i class="el-icon-star-on">&nbsp;Like</i>&nbsp;&nbsp;&nbsp;{{
+                toRaw(dialogData[0].collect)
+              }}</el-button
+            >
           </div>
         </div>
         <div class="dialogImg">
-          <div class="main_img"></div>
+          <div class="main_img">
+            <img :src="toRaw(dialogData[0].imageUrl)" alt="" />
+          </div>
         </div>
         <div class="dialogContent">
-          黑塞说过一句富有哲理的话，有勇气承担命运这才是英雄好汉。这似乎解答了我的疑惑。
-          米歇潘曾经说过，生命是一条艰险的峡谷，只有勇敢的人才能通过。带着这句话，我们还要更加慎重的审视这个问题：
-          可是，即使是这样，访问来源的比例的出现仍然代表了一定的意义。
-          从这个角度来看， 经过上述讨论，
-          所谓访问来源的比例，关键是访问来源的比例需要如何写。
-          我们不得不面对一个非常尴尬的事实，那就是， 既然如此， 我认为，
-          访问来源的比例因何而发生？
-          我们一般认为，抓住了问题的关键，其他一切则会迎刃而解。
-          我们一般认为，抓住了问题的关键，其他一切则会迎刃而解。
-          达尔文说过一句富有哲理的话，敢于浪费哪怕一个钟头时间的人，说明他还不懂得珍惜生命的全部价值。这似乎解答了我的疑惑。
-          普列姆昌德在不经意间这样说过，希望的灯一旦熄灭，生活刹那间变成了一片黑暗。这不禁令我深思。
-          可是，即使是这样，访问来源的比例的出现仍然代表了一定的意义。
-          我们都知道，只要有意义，那么就必须慎重考虑。 从这个角度来看，
-          既然如何，
-          了解清楚访问来源的比例到底是一种怎么样的存在，是解决一切问题的关键。
-          我认为， 本人也是经过了深思熟虑，在每个日日夜夜思考这个问题。
-          问题的关键究竟为何？ 那么， 问题的关键究竟为何？
-          了解清楚访问来源的比例到底是一种怎么样的存在，是解决一切问题的关键。
-          塞涅卡曾经说过，生命如同寓言，其价值不在与长短，而在与内容。这似乎解答了我的疑惑。
-          既然如何，
-          迈克尔·F·斯特利曾经提到过，最具挑战性的挑战莫过于提升自我。这句话语虽然很短，但令我浮想联翩。
-          访问来源的比例因何而发生？ 那么，
-          佚名曾经提到过，感激每一个新的挑战，因为它会锻造你的意志和品格。这不禁令我深思。
-          既然如何， 一般来说， 而这些并不是完全重要，更加重要的问题是，
-          了解清楚访问来源的比例到底是一种怎么样的存在，是解决一切问题的关键。
+          {{ toRaw(dialogData[0].description) }}
         </div>
       </div>
     </el-dialog>
@@ -129,9 +138,10 @@
 </template>
 
 <script>
-import { onMounted, toRefs, reactive, ref, computed } from "vue";
+import { ElMessage } from "element-plus";
+import { toRaw } from "@vue/reactivity";
+import { onMounted, toRefs, reactive, ref } from "vue";
 import { getPlazaData } from "../api/Plaza";
-import Fuse from "fuse.js";
 export default {
   data() {
     return {
@@ -141,48 +151,44 @@ export default {
           name: "所有",
         },
         {
-          value: "views",
-          name: "数据可视化",
+          value: "economics",
+          name: "经济",
         },
         {
-          value: "pic",
-          name: "图表类型",
+          value: "farming",
+          name: "农业",
         },
         {
-          value: "icon",
-          name: "图标组合",
+          value: "weather",
+          name: "气候",
         },
         {
-          value: "wealth",
-          name: "学习资源",
+          value: "energy",
+          name: "能源",
         },
         {
-          value: "technology",
-          name: "技术支持",
+          value: "education",
+          name: "教育",
         },
         {
-          value: "technology",
-          name: "技术支持",
+          value: "health",
+          name: "健康",
         },
         {
-          value: "technology",
-          name: "技术支持",
+          value: "gender",
+          name: "性别",
         },
         {
-          value: "technology",
-          name: "技术支持",
+          value: "environment ",
+          name: "环境",
         },
         {
-          value: "technology",
-          name: "技术支持",
+          value: "infrastructure",
+          name: "基础建设",
         },
         {
-          value: "technology",
-          name: "技术支持",
-        },
-        {
-          value: "technology",
-          name: "技术支持",
+          value: "society ",
+          name: "社会保障",
         },
       ],
       dataList2: [
@@ -192,38 +198,32 @@ export default {
       selectNode: "",
     };
   },
-  components: {},
-
   setup() {
-    // const router = useRouter();
     const data = reactive({
       infoData: {},
       show: false,
       showDialog: false,
+      dialogData: [],
+      selectedIndex: 0,
     });
     let searchText = ref("");
     onMounted(() => {
       getPlazaData({}).then((res) => {
         data.infoData = res.result;
-        data.dialogData = res.result;
         data.show = !data.show;
       });
     });
-    //使用fuse组件实现模糊搜索
-    const searchList = computed(() => {
-      if (searchText.value.trim()) {
-        return new Fuse(data.infoData, { keys: ["title", "author"] })
-          .search(searchText.value.trim())
-          .map((e) => e.item);
-      } else {
-        return data.infoData;
-      }
-    });
-
+    //搜索
+    function search() {
+      getPlazaData({ type: searchText }).then((res) => {
+        data.infoData = res.result;
+      });
+    }
     //改变类型从接口获取数据,
-    const changeType = (value) => {
+    const changeType = (value, index) => {
       getPlazaData({ type: value }).then((res) => {
         data.infoData = res.result;
+        data.selectedIndex = index;
       });
     };
     //下拉框做排序
@@ -245,29 +245,37 @@ export default {
     const clickItem = (value) => {
       getPlazaData({ id: value })
         .then((res) => {
-          console.log("这是res的值");
-          console.log(res.result);
-
-          let a = [];
-          a = res.result;
-          console.log("这是res.result赋值给a 的值");
-          console.log(a);
-          console.log("这是res.result赋值给 data.dialogData的值");
           data.dialogData = res.result;
-          console.log(data.dialogData);
         })
         .catch(() => {
           {
-            alert("接口调用错误");
+            alert("接口调用错误!");
           }
         });
       data.showDialog = true;
     };
+    //喜欢按钮事件
+    const like = (value) => {
+      getPlazaData({ likeID: value })
+        .then((res) => {
+          data.dialogData = res.result;
+          ElMessage.success({
+            message: "Like成功！",
+          });
+        })
+        .catch(() => {
+          ElMessage.error({
+            message: "接口调用错误！",
+          });
+        });
+    };
     return {
+      toRaw,
       clickItem,
       changeSort,
       changeType,
-      searchList,
+      like,
+      search,
       searchText,
       ...toRefs(data),
     };
@@ -295,13 +303,13 @@ export default {
       height: 5vh;
       display: inline-block;
       width: 10%;
+      vertical-align: middle;
     }
     .bar {
+      vertical-align: middle;
       white-space: nowrap;
-      vertical-align: top;
-      overflow-x: auto;
       height: 100%;
-      width: 80%;
+      width: 60%;
       display: inline-block;
       height: 50px;
       ul {
@@ -315,32 +323,42 @@ export default {
           cursor: pointer;
           color: #2f80ed;
         }
+        .liStyle.active {
+          border-bottom: #66a4f5 solid 2px;
+        }
+      }
+    }
+    .searchpack {
+      vertical-align: middle;
+      height: 5vh;
+      width: auto;
+      align-items: center;
+      display: inline-flex;
+      .search {
+        height: 25px;
+        width: 15vw;
+        border-radius: 15px;
+        background: #eef2f8;
+        text-align: center;
+
+        .input {
+          vertical-align: top;
+          text-align: right;
+          color: #757575;
+          font-size: 12px;
+          width: 85%;
+          height: 100%;
+          background: #eef2f8;
+          outline: none;
+        }
       }
     }
   }
 
-  .searchpack {
-    height: 5vh;
-    width: auto;
-    align-items: center;
-    display: inline-flex;
-    .search {
-      height: 25px;
-      width: 25vw;
-      border-radius: 15px;
-      background: #eef2f8;
-      text-align: center;
-      .input {
-        text-align: right;
-        color: #757575;
-        font-size: 12px;
-        width: 85%;
-        height: 100%;
-        display: inline-block;
-        background: #eef2f8;
-        outline: none;
-      }
-    }
+  .divBodynull {
+    margin-top: 10%;
+    width: 100%;
+    height: 85vh;
   }
   .divBody {
     overflow: auto;
@@ -351,6 +369,7 @@ export default {
     flex-wrap: wrap;
     align-items: center;
     justify-content: space-around;
+    align-content: flex-start;
     .view {
       cursor: pointer;
       margin-top: 3vh;
@@ -388,7 +407,7 @@ export default {
       }
     }
   }
-  /deep/.el-dialog {
+  :deep(.el-dialog) {
     border-radius: 20px;
     background: #e9ebf0;
     min-width: 880px;
@@ -397,7 +416,6 @@ export default {
     color: #233062;
     height: 85vh;
     width: 100%;
-
     .dialogTitle {
       margin: 0 auto;
       height: auto;
@@ -418,12 +436,18 @@ export default {
           border-radius: 50%;
           float: right;
           margin-right: 1vw;
+          img {
+            height: 100%;
+            width: auto;
+            margin: 0 auto;
+          }
         }
       }
       .author {
         flex: 1.5;
         height: 10vh;
         .author_name {
+          font-size: 28px;
           height: 60%;
           padding: 10px 0;
         }
@@ -434,6 +458,7 @@ export default {
         }
       }
       .message {
+        font-size: 28px;
         flex: 4;
         height: 10vh;
         .message_title {
@@ -450,40 +475,42 @@ export default {
         }
       }
       .like {
-        position: relative;
         vertical-align: middle;
         flex: 3;
         height: 10vh;
         .button {
-          position: absolute;
-          top: 50%;
-          left: 60%;
-          margin: -20px 0 0 0;
+          margin-top: 4%;
           border-radius: 20px;
-          height: 40px;
-          width: 140px;
-          background: #2f80ed;
+          width: 150px;
+          height: 50%;
+          float: right;
           i {
+            font-size: 16px;
             color: #eef2f8;
-            font-size: 25px;
-            margin: 8px 25px;
           }
         }
       }
     }
     .dialogImg {
-      margin-top: 2vh;
-      height: 60%;
-      width: 85%;
-      margin: 2vh auto;
-      background: #757575;
+      width: 95%;
+      margin: 0 auto;
       .main_img {
+        margin: 1% auto;
+        width: 88%;
+        height: auto;
+        max-height: 58vh;
+        overflow: scroll;
+        img {
+          width: 100%;
+          height: 100%;
+          max-height: 100%;
+        }
       }
     }
     .dialogContent {
-      height: auto;
+      height: 18%;
       width: 85%;
-      margin: 0 auto;
+      margin: 1% auto;
       text-indent: 32px;
       overflow: scroll;
     }
